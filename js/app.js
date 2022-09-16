1501e82addffab6034bf3924f1144c825ef785df
SaSaChu <teresa7397@gmail.com>
2022-09-16 17:54:20 +0800

    刪說明文

--- js/app.js
+++ js/app.js
@@ -1,11 +1,4 @@
-/*
-Template Name: Lexa - Responsive Bootstrap 4 Admin Dashboard
-Author: Themesbrand
-Version: 2.0.0
-Website: https://themesbrand.com/
-Contact: themesbrand@gmail.com
-File: Main Js File
-*/
+
 
 
 (function ($) {
